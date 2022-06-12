00c83c0c8a20bf5deae869491d747c71c81555c6
DeveloperAspire <franklinokolie86@gmail.com>
2022-06-12 13:34:32 +0100

    chore: added type module for images

--- template-react-ts/parcel.d.ts
+++ template-react-ts/parcel.d.ts
@@ -10,3 +10,7 @@ declare module "*.sass" {
   const value: Record<string, string>;
   export default value;
 }
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.jpeg";
+declare module "*.svg";
